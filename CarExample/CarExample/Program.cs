11ae84a1b3ad60a820e6ae06ee7fc9640ed1650b
Brandon Ma <46719791+Accentian@users.noreply.github.com>
2022-07-24 16:38:36 -0600

    Add files via upload

---
+++ CarExample/CarExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CarExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("red");
+            car1.Color = "green";
+            Console.WriteLine(car1.Describe());
+
+            Car car2 = new Car("blue");
+            Console.WriteLine(car2.Describe());
+
+            // Pause
+            Console.ReadLine();
+
+        } // End main
+
+        class Car
+        {
+            private string color;
+            
+            // Constructors
+            public Car (string color)
+            {
+                this.color = color;
+            }
+
+            public string Color     // Setters and getters
+            {
+                get { return color; }
+                set { color = value; }
+            }
+
+            // Create your own method to display the color of the car
+            public string Describe()
+            {
+                return "This car color is " + color;
+            }
+
+
+        } // End Car class
+
+    } // End class
+} // End namespace
---
+++ UnderstandingScope/UnderstandingScope/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace UnderstandingScope
+{
+    class Program
+    {
+        // Declare statics
+        private static string k;
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                k = i.ToString();
+            } // End for
+
+
+            // Console.WriteLine(i);    Error resides outside for loop
+            MessageK();
+
+            // Test our new class
+            MoreMessages mm = new MoreMessages();
+            mm.NewMessage();
+            // Change private to public in order to access it outside of the class
+
+            // Put this line in to pause the program
+            Console.ReadLine();
+
+        } // End main
+
+        static void MessageK()
+        {
+            Console.WriteLine("Value of variable k is " + k);
+        }
+
+    } // End class
+
+
+    // Create a new class
+    class MoreMessages
+    {
+        public void NewMessage()
+        {
+            Console.WriteLine("New Message from MoreMessage Class");
+
+        }
+
+        private void AdditionalMessage()
+        {
+            Console.WriteLine("Additional Message");
+
+        }
+
+    } // End class MoreMessages
+
+
+} // End namespace
