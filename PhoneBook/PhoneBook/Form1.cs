ccbf30b878fc33a4bcd0b98e29828aeba30045e3
Brandon Ma <46719791+Accentian@users.noreply.github.com>
2022-07-24 16:39:19 -0600

    Add files via upload

---
+++ PhoneBook/PhoneBook/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneBook
+{
+    public partial class frmBook : Form
+    {
+        public frmBook()
+        {
+            InitializeComponent();
+        }
+
+        private void personBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.personBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.personDataSet);
+
+        }
+
+        private void frmBook_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personDataSet.Person' table. You can move, or remove it, as needed.
+            this.personTableAdapter.Fill(this.personDataSet.Person);
+
+        }
+    }
+}
