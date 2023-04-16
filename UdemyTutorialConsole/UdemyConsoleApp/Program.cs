55a2b4f1e8ff6cf43579ff3dfdc064de3dd0fd01
Brandon Ma <46719791+Accentian@users.noreply.github.com>
2023-04-16 19:41:48 -0400

    Add files via upload

---
+++ UdemyTutorialConsole/UdemyConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int myNum = 6;
+double myDouble = 5.67;
+string myString = "This is a string!";
+bool myBool = true;
+char myChar = 'B';
+
+
+// We can also use placeholders or add empty as seen below.
+Console.WriteLine("The value of my number is {0, -6} and takes up -6 spaces. The value of my double is {1, 7} and take up 7 spaces. Also, {2}", myNum, myDouble, myString);
+
+Console.WriteLine();
+
+Console.WriteLine("My character is " + myChar + " and that is " + myBool);
---
+++ UdemyZipcodeArray/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] zipcode = { "29594", "10000", "25940", "18584", "66666", "12345" };
+Console.WriteLine("Enter a zipcode");
+string input = Console.ReadLine();
+
+bool found = false;
+
+for (int i = 0;i < zipcode.Length; i++)
+{
+    if (input == zipcode[i])
+    {
+        found = true;
+        break;
+    }
+}
+if (found)
+{
+    Console.WriteLine("Zipcode found!");
+}
+else
+{
+    Console.WriteLine("Zipcode not found!");
+}
+    
+    
